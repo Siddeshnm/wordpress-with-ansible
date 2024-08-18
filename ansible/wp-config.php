e102ad7cfa5e63e58a3140ee6d3c6f1cc6717e7e
Siddeshnm <siddeshnm1@gmail.com>
2024-08-18 19:13:32 +0000

    wordpress with ansible

---
+++ ansible/wp-config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_NAME', 'Wordpress');
+define('DB_USER', 'wordpress');
+define('DB_PASSWORD', 'Wordpress@1234');
+define('DB_HOST', '10.0.0.37');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
