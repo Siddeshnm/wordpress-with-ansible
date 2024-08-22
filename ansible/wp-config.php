0cd498aa4efff179ee36dccf8f2761308f3fb3fc
Siddeshnm <siddeshnm1@gmail.com>
2024-08-22 18:22:14 +0000

    updated on 22-08

--- ansible/wp-config.php
+++ ansible/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'wordpress' );
 define( 'DB_PASSWORD', 'Wordpress@1234' );
 
 /** Database hostname */
-define( 'DB_HOST', '10.0.0.40' );
+define( 'DB_HOST', '10.0.0.37' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
